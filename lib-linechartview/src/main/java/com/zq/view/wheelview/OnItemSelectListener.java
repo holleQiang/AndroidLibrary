476fe73b00d03e7d6441157dc18d417236c2d8d0
zhangqiang <zhangqiang@youyuwo.com>
2017-11-16 15:31:51 +0800

    asdad

---
+++ lib-linechartview/src/main/java/com/zq/view/wheelview/OnItemSelectListener.java
@@ -0,0 +1,10 @@
+package com.zq.view.wheelview;
+
+/**
+ * Created by zhangqiang on 2017/8/11.
+ */
+
+public interface OnItemSelectListener {
+
+    void onItemSelect(int position, int firstVisiblePosition, int lastVisiblePosition);
+}
