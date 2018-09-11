e01a3214b228238938aecb888ecc4758880bbb02
holleQiang <852286406@qq.com>
2018-09-11 13:40:04 +0800

    ss

--- lib-utils/src/main/java/com/zq/utils/StringUtil.java
+++ lib-utils/src/main/java/com/zq/utils/StringUtil.java
@@ -1,7 +1,37 @@
 package com.zq.utils;
 
+import android.content.Context;
+import android.support.annotation.StringRes;
+
+import java.util.Locale;
+
 /**
  * Created by zhangqiang on 2018/7/19.
  */
 public class StringUtil {
+
+    /**
+     * 格式化
+     *
+     * @param context
+     * @param stringResId   字符串资源id
+     * @param replaceValues 替换的值
+     * @return 格式化后的字符串
+     */
+    public static String format(Context context, @StringRes int stringResId, Object... replaceValues) {
+
+        return format(context.getResources().getString(stringResId), replaceValues);
+    }
+
+    /**
+     * 格式化
+     *
+     * @param text   字符串资源id
+     * @param replaceValues 替换的值
+     * @return 格式化后的字符串
+     */
+    public static String format(String text, Object... replaceValues) {
+
+        return String.format(Locale.CHINA, text, replaceValues);
+    }
 }
--- lib-utils/src/main/java/com/zq/utils/ViewUtil.java
+++ lib-utils/src/main/java/com/zq/utils/ViewUtil.java
@@ -1,6 +1,13 @@
 package com.zq.utils;
 
 import android.content.Context;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.ClickableSpan;
+import android.widget.TextView;
+
+import com.zq.utils.text.FixedLinkMovementMethod;
+import com.zq.utils.text.HtmlTextHelper;
 
 /**
  * Created by zhangqiang on 2018/7/19.
@@ -18,4 +25,32 @@ public class ViewUtil {
         float density = context.getResources().getDisplayMetrics().density;
         return (int) (density * dp + 0.5f);
     }
+
+    /**
+     * 为textView设置html文本
+     */
+    public static void setHtmlText(TextView textView, String source) {
+
+        setHtmlText(textView, source, null);
+    }
+
+    /**
+     * 为textView设置html文本
+     */
+    public static void setHtmlText(TextView textView, String source, com.caiyi.fund.trade.widget.span.URLSpanWrapper.OnClickListener onClickListener) {
+
+        if (TextUtils.isEmpty(source)) {
+            textView.setText(source);
+            return;
+        }
+        Spanned spanned = new HtmlTextHelper().fromHtml(source, textView.getContext(), onClickListener);
+        if (spanned != null) {
+            Object[] spans = spanned.getSpans(0, spanned.length(), ClickableSpan.class);
+            if (spans != null && spans.length > 0) {
+                textView.setMovementMethod(FixedLinkMovementMethod.getInstance());
+            }
+        }
+        textView.setText(spanned);
+    }
+
 }
